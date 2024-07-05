eb994eadf15c63d1b147bd5d25cd633f0c59b223
mgsviryd <mgsviryd@gmail.com>
2024-07-05 19:21:32 +0300

    WebSecurityConfig: add

---
+++ src/main/java/com/sviryd/chat/config/WebSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.sviryd.chat.config;
+
+import com.sviryd.chat.provider.AuthProvider;
+import com.sviryd.chat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public AuthProvider authProvider() {
+        AuthProvider authProvider = new AuthProvider();
+        authProvider.setUserService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/auth").permitAll()
+                                .requestMatchers("/init").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/messages").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/messages").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        http.authenticationProvider(authProvider());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/js/**", "/images/**");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+}
