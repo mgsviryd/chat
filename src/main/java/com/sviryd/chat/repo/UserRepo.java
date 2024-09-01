07c1c5abbef3d3af64e74d356967f991fc9311dc
mgsviryd <mgsviryd@gmail.com>
2024-09-01 10:10:35 +0300

    task

--- src/main/java/com/sviryd/chat/repo/UserRepo.java
+++ src/main/java/com/sviryd/chat/repo/UserRepo.java
@@ -2,10 +2,7 @@ package com.sviryd.chat.repo;
 
 
 import com.sviryd.chat.domain.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
